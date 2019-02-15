17aa824780f4b8820ff110af878f1b8ebb5a0d5b
LAPTOP-O6BGLDO0\Verbis-IT <t.versantvoort@hotmail.com>
2019-02-15 18:18:07 +0100

    Initial commit

---
+++ SQL Timed Service/SQL Timed Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQL_Timed_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
